9ae83f0f72082c71ce9e1731167fc5a6e09bc800
CristianAlvarez2003 <cris0192776@gmail.com>
2021-09-27 19:22:37 -0600

    Funciona al 75%

---
+++ app/src/main/java/com/example/crub_v2_practica/ventana1.kt
@@ -0,0 +1,11 @@
+package com.example.crub_v2_practica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ventana1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana1)
+    }
+}
